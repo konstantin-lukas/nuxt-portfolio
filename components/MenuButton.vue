4ac91aa1de1c5d17f40f44bd89df27e1d9e049d3
konstantin <konstantin_lukas@web.de>
2024-08-08 21:10:49 +0200

    add menu button

--- components/MenuButton.vue
+++ components/MenuButton.vue
@@ -1,14 +1,62 @@
 <script setup lang="ts">
-
+import { ref } from 'vue';
+const open = ref(false);
 </script>
 
 <template>
-    <div>
-        <span/>
-        <span/>
-    </div>
+    <button
+        :class="{ open }"
+        @click="open = !open"
+    >
+        <span>
+            <span/>
+            <span/>
+        </span>
+    </button>
 </template>
 
 <style scoped lang="scss">
-
+@import "../assets/mixins.scss";
+button {
+    position: fixed;
+    top: 2rem;
+    right: 2rem;
+    width: 2rem;
+    height: 1.75rem;
+    & > span {
+        height: 100%;
+        width: 100%;
+        transform: skewX(-25deg);
+        display: block;
+        position: relative;
+        @include transition(transform);
+        & > span {
+            @include transition(all);
+            display: block;
+            height: 100%;
+            width: var(--border-width);
+            background-color: var(--pink);
+            position: absolute;
+            left: 0;
+            top: 0;
+            transform: translateX(0.5rem);
+            &:last-of-type {
+                left: initial;
+                right: 0;
+                transform: translateX(-0.5rem);
+            }
+        }
+    }
+    &.open > span {
+        transform: none;
+        & > span:first-of-type {
+            transform: skewX(-45deg) translateX(calc(1rem - var(--border-width) / 2));
+            width: calc(0.5px + var(--border-width));
+        }
+        & > span:last-of-type {
+            transform: skewX(45deg) translateX(calc(-1rem + var(--border-width) / 2));
+            width: calc(0.5px + var(--border-width));
+        }
+    }
+}
 </style>
