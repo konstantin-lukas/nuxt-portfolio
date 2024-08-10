a5836e1ddc58f8c690dd253ff0c1f64bb106dd3d
konstantin <konstantin_lukas@web.de>
2024-08-10 16:35:55 +0200

    add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import projects from "./assets/scripts/projects";
+import type { SitemapUrlInput } from "#sitemap";
+
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
@@ -8,10 +11,12 @@ export default defineNuxtConfig({
         "~/assets/styles/variables.scss",
         "~/assets/styles/animations.scss",
     ],
-    modules: ["@nuxt/fonts", "@nuxt/image"],
+    modules: ["@nuxt/fonts", "@nuxt/image", "@nuxtjs/sitemap"],
     routeRules: {
         "/": { prerender: true },
         "/projekte": { prerender: true },
+        "/impressum": { prerender: true },
+        "/datenschutz": { prerender: true },
     },
     fonts: {
         families: [{
@@ -21,5 +26,40 @@ export default defineNuxtConfig({
             styles: ["normal"],
             subsets: ["latin", "latin-ext"],
         }],
+    },
+    runtimeConfig: {
+        public: {
+            siteUrl: "https://konstantinlukas.de",
+        },
+    },
+    site: {
+        url: "https://konstantinlukas.de",
+        name: "Konstantin Lukas | Full-Stack Developer",
+    },
+    sitemap: {
+        defaults: {
+            changefreq: "monthly",
+            lastmod: new Date(),
+        },
+        excludeAppSources: true,
+        urls: () => {
+            const routes = [
+                {
+                    priority: 1,
+                    url: "https://konstantinlukas.de",
+                    changefreq: "never",
+                },
+                {
+                    priority: 0.9,
+                    url: "https://konstantinlukas.de/projekte",
+                    changefreq: "monthly",
+                },
+            ];
+            return [...routes, ...projects.map(p => ({
+                priority: 0.8,
+                loc: `https://konstantinlukas.de/projekte/${p.name}`,
+                changefreq: "never",
+            }))] as SitemapUrlInput[];
+        },
     },
 });
