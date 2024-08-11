5a0a1b0b714cbca853e5ef143d4cfeca00141e1e
Konstantin Lukas <konstantin_lukas@web.de>
2024-08-11 10:52:39 +0200

    implement tower setup and positioning

--- components/JengaTower.vue
+++ components/JengaTower.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {
-    BoxGeometry,
+    AmbientLight,
     DirectionalLight,
     Mesh,
     MeshStandardMaterial,
@@ -9,12 +9,21 @@ import {
     Scene,
     WebGLRenderer,
 } from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { RoundedBoxGeometry } from "assets/scripts/rounded";
 
 const sceneContainer = ref(null);
+const restart = ref(false);
+
+const blockWidth = 2.5;
+const blockHeight = 1.5;
+const blockDepth = 7.5;
+const layers = 18;
 
 function initScene() {
     if (sceneContainer.value === null) return;
+    restart.value = false;
+    (sceneContainer.value as HTMLDivElement).innerHTML = "";
     const scene = new Scene();
 
     const aspect = window.innerWidth / window.innerHeight;
@@ -27,19 +36,25 @@ function initScene() {
         1,
         1000,
     );
-    /* const camera = new PerspectiveCamera(
-        90,
-        aspect,
-    ); */
-    camera.position.set(200, 100, 80);
-    camera.lookAt(0, 20, 0);
 
-    const light = new DirectionalLight("#ffffff", 1);
+    const horizontalThird = (2 * cameraSize * aspect) / 6;
+    const verticalThird = (2 * cameraSize) / 3;
+    camera.position.set(20, 25, 10);
+    camera.lookAt(0, 10, 0);
+    camera.translateX(-horizontalThird);
+    camera.translateY(verticalThird - blockHeight * (18 - 2) / 2);
+
+    const light = new DirectionalLight(0xFFFFFF, 1);
     light.position.set(5, 3, 1);
     light.target.position.set(0, 0, 0);
     light.castShadow = true;
     scene.add(light);
     scene.add(light.target);
+
+    const ambience1 = new AmbientLight(0xFFFFFF, 1);
+    scene.add(ambience1);
+    const ambience2 = new AmbientLight(0xFAE0A3, 1);
+    scene.add(ambience2);
 
     /* const lightHelper = new DirectionalLightHelper(light);
     lightHelper.color = "#000000";
@@ -48,39 +63,35 @@ function initScene() {
     const renderer = new WebGLRenderer({ antialias: true });
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = PCFSoftShadowMap;
-    renderer.setClearColor("#ffffff", 0);
+    renderer.setClearColor(0xFFFFFF, 0);
 
-    const controls = new OrbitControls(camera, renderer.domElement);
+    /* const controls = new OrbitControls(camera, renderer.domElement);
     controls.enablePan = true;
     controls.enableZoom = true;
-    controls.update();
+    controls.update(); */
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     (sceneContainer.value as HTMLDivElement).appendChild(renderer.domElement);
-    camera.position.z = 10;
 
     generateTower(scene);
 
     const animate = () => {
+        if (restart.value) return;
+        // controls.update();
         requestAnimationFrame(animate);
-        controls.update();
-        // console.log("Camera Position:", camera.position);
-        // console.log("Camera Rotation:", camera.rotation);
         renderer.render(scene, camera);
     };
     animate();
 }
 
 function generateTower(scene: Scene) {
-    const blockWidth = 2.5;
-    const blockHeight = 1.5;
-    const blockDepth = 7.5;
-
-    const geometry = new BoxGeometry(blockWidth, blockHeight, blockDepth);
-    const material = new MeshStandardMaterial({ color: 0xB58868 });
+    const geometry = new RoundedBoxGeometry(blockWidth, blockHeight, blockDepth, 3, 0.1);
+    const material = new MeshStandardMaterial({
+        color: 0xFAE0A3,
+    });
+    // const material = new MeshToonMaterial({ color: 0xFFFFFF });
 
     let stackHeight = 0;
-    const layers = 18;
     for (let i = 0; i < layers; i++) {
         let previousRandom = 0;
         for (let j = 0; j < 3; j++) {
@@ -101,8 +112,17 @@ function generateTower(scene: Scene) {
     }
 }
 
+function resetAnimation() {
+    restart.value = true;
+    initScene();
+}
+
 onMounted(() => {
     initScene();
+    window.addEventListener("resize", resetAnimation);
+});
+onBeforeUnmount(() => {
+    window.removeEventListener("resize", resetAnimation);
 });
 </script>
 
