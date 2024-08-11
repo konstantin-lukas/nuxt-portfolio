0701cf9f3c6b2ae5022cecd1af03f58dedbb1fd1
Konstantin Lukas <konstantin_lukas@web.de>
2024-08-11 21:46:05 +0200

    update project descriptions

--- assets/scripts/projects.ts
+++ assets/scripts/projects.ts
@@ -8,10 +8,12 @@ const projects: Project[] = [
         context: "SHK (HU zu Berlin)",
         type: "Webseite",
         about:
-            "Die Zitatsuchmaschine wurde am Lehrstuhl für Maschinelles Lernen der Humboldt-Universität zu Berlin "
-            + "entwickelt. Ich war allein verantwortlich für die Neugestaltung und Implementierung der Webseite. Die "
-            + "Extraktion und Verarbeitung der Zitate sowie die Pflege der Datenbank wurden von meinen Kollegen "
-            + "übernommen.",
+            "Die an der Humboldt-Universität zu Berlin entwickelte Zitatsuchmaschine ermöglicht es Nutzern, Zitate aus "
+            + "dem Web zu extrahieren und gezielt nach Zitaten von bestimmten Personen oder zu spezifischen Themen zu "
+            + "suchen. In diesem Projekt war ich verantwortlich für die Neugestaltung, Implementierung und das "
+            + "Deployment der Webseite, während meine Kollegen das Crawlen und Extrahieren der Zitate sowie die Pflege "
+            + "der Datenbank übernommen haben. Die Seite habe ich mit Next.js entwickelt und mit PM2 deployt. Leider "
+            + "ist dieses Projekt nicht Open Source, aber die Seite kann über den untenstehenden Link besucht werden.",
         links: [
             {
                 text: "Besuchen",
@@ -26,10 +28,13 @@ const projects: Project[] = [
         context: "Hobbyprojekt",
         type: "Desktop-App",
         about:
-            "Oslo ist eine Electron-App für die persönliche Finanzverwaltung. "
-            + "Ursprünglich habe ich dieses Programm in Python geschrieben, "
-            + "später allerdings dann neu als Electron-App aufgesetzt und "
-            + "dann nochmals später mit Electron und React implementiert.",
+            "Oslo ist eine von mir entwickelte Desktop-App zur persönlichen Finanzverwaltung. Die Daten werden dabei "
+            + "ausschließlich in einer lokalen SQLite-Datenbank auf dem Rechner des Nutzers gespeichert und niemals an"
+            + "Dritte weitergegeben. Das Projekt habe ich mit Electron und React umgesetzt. Der Quellcode ist"
+            + "vollständig öffentlich und über den untenstehenden Link zugänglich. Die App befindet sich bereits in der"
+            + "dritten Version. Ursprünglich habe ich sie in Python entwickelt, später mit Electron und Vanilla "
+            + "JavaScript neu geschrieben und schließlich in React umgesetzt. Durch die Umsetzung desselben Projekts "
+            + "mit drei verschiedenen Technologien konnte ich wertvolle Erfahrungen sammeln.",
         links: [
             {
                 text: "GitHub",
@@ -44,9 +49,14 @@ const projects: Project[] = [
         context: "Hobbyprojekt",
         type: "Library",
         about:
-            "Anzol ist eine Library für React, die Hooks bereitstellt, mit denen man kürzeren und verständlicheren"
-            + "Code schreiben kann. Das GitHub-Repository beinhaltet CI/CD für GitHub-Releases, Releases auf NPM und "
-            + "GitHub Pages für die Dokumentation.",
+            "Anzol ist eine JavaScript-Bibliothek für React, die nützliche Hooks wie useFetch bereitstellt, welche "
+            + "leider weder in Standard-React noch in Next.js enthalten sind. Vor Anzol habe ich bereits eine andere "
+            + "JavaScript-Bibliothek entwickelt. Anders als bei dem vorherigen Projekt, habe ich dieses mal gelernt, "
+            + "wie man eine Bibliothek speziell für React erstellt und wie man die notwendigen GitHub Actions für "
+            + "CI/CD einrichtet. Wenn ein neuer Tag gepusht wird, erstellt das System automatisch ein neues Release "
+            + "auf GitHub und lädt eine aktualisierte Version auf NPM hoch. Das Projekt umfasst zudem automatisierte "
+            + "Tests, um sicherzustellen, dass alles reibungslos funktioniert. Das GitHub-Repository ist unten"
+            + "verlinkt.",
         links: [
             {
                 text: "GitHub",
@@ -61,10 +71,15 @@ const projects: Project[] = [
         context: "Ausbildung",
         type: "Webseite",
         about:
-            "Nox ist eine junge Metal-Band aus Berlin. "
-            + "Ich habe ihr Logo gestaltet und ihren Webauftritt implementiert. "
-            + "Das Ganze ist durch eine MySQL-Datenbank gestützt, die es der Band möglich macht, "
-            + "die Seite über das von mir geschriebene CMS stets aktuell zu halten.",
+            "Nox ist eine Metalband aus Berlin, für die ich im Rahmen meiner Ausbildung bei Zalando eine Webseite "
+            + "sowie ein Logo gestaltet habe. Dieses Projekt war mein erstes größeres Vorhaben, das ich für einen "
+            + "externen Auftraggeber realisiert habe. Allein die Planungsphase für die Webseite und das Logo erstreckte "
+            + "sich über mehrere Wochen und erforderte zahlreiche Meetings mit der Band. Nachdem ich das Logo und die "
+            + "Screendesigns in Adobe Illustrator entworfen hatte, begann die Implementierung. Die Webseite hat "
+            + "bewusst wenige Abhängigkeiten und enthält daher viele maßgeschneiderte Lösungen, einschließlich eines "
+            + "komplett selbst entwickelten CMS. Damals habe ich die Seite in PHP ohne die Unterstützung eines "
+            + "Frameworks umgesetzt. Heute würde ich einen anderen Ansatz wählen, was jedoch zeigt, wie viel ich seit"
+            + "dem gelernt habe.",
         links: [
             {
                 text: "Besuchen",
@@ -79,9 +94,13 @@ const projects: Project[] = [
         context: "Hobbyprojekt",
         type: "Library",
         about:
-            "Hierbei handelt es sich um eine in TypeScript geschriebene Library, "
-            + "die sich an einen normalen Text-Input hängt und diesen so manipuliert, "
-            + "dass immer ein korrekt formattierter Währungsstring angezeigt wird.",
+            "Irgendwann entwickelte ich ein starkes Interesse daran, eine eigene Library zu schreiben, und begann zu "
+            + "überlegen, was ein angemessener Umfang für ein erstes Projekt dieser Art sein könnte. Schließlich kam "
+            + "ich auf die Idee, eine Library zu entwickeln, die Währungsinputs erstellt – also Zahlenfelder, die ihre "
+            + "Inhalte als Währungsstrings formatieren. Ursprünglich habe ich das Projekt in reinem JavaScript "
+            + "umgesetzt. Da dies mittlerweile nicht mehr meinen persönlichen Qualitätsstandards entspricht und das "
+            + "Projekt dennoch einige Sterne sammeln konnte, habe ich es komplett neu in TypeScript geschrieben und "
+            + "automatisierte Tests mit Jest hinzugefügt.",
         links: [
             {
                 text: "GitHub",
@@ -96,14 +115,19 @@ const projects: Project[] = [
         context: "Freizeit & Uni",
         type: "Videospiel",
         about:
-            "Das hier ist ein kleines Spiel in dem man Geister mit seiner Taschenlampe bekämpft. "
-            + "Wobei das Wort \"klein\" nicht ganz zutrifft, denn das Spiel ist ohne ein richtiges "
-            + "Framework nur mit der SDL-Library entwickelt und der Source-Code ist ziemlich umfangreich. "
-            + "Da ich allerdings noch nicht weiß, ob ich das Projekt noch mal aufnehmen werde, "
-            + "ist der Source-Code nicht offen verfügbar.",
+            "Im Rahmen meines Computergrafik-Kurses an der Universität mussten wir ein selbstgewähltes Projekt "
+            + "entwickeln. Ich hatte bereits zuvor an einem kleinen Spiel gearbeitet, in dem der Spieler Geister mit "
+            + "einer Taschenlampe vertreiben muss. Ursprünglich habe ich dieses Projekt begonnen, um C++ zu lernen, "
+            + "nachdem ich Effective Modern C++ von Scott Meyers gelesen hatte. Für den Kurs setzte ich die "
+            + "Entwicklung des Spiels fort. Das Spiel wurde mit SDL2 entwickelt, einer Library, die es ermöglicht, "
+            + "Texturen zu rendern und Controller-Eingaben zu nutzen. Im Gegensatz zu einer ausgearbeiteten "
+            + "Spiele-Engine wie Unreal musste ich sämtliche Spielmechaniken selbst implementieren. Auch das Erstellen "
+            + "von Texturen und Animationen musste ich erstmal lernen und hat mich viel Mühe gekostet. Da ich "
+            + "unsicher bin, ob ich das Spiel in Zukunft weiterentwickeln werde, ist es derzeit nicht Open Source, "
+            + "aber unten finden Sie einen Link zu einem kurzen Demovideo.",
         links: [
             {
-                text: "Demo Video",
+                text: "Gameplay",
                 url: "/videos/untitled_ghost_game.mp4",
             },
         ],
